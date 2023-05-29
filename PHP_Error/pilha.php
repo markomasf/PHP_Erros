04696ba1dbac37ba4c973e263000222e3d50649a
Marco Antônio <markomasf@gmail.com>
2023-05-29 17:05:26 -0300

    erro exception

--- PHP_Error/pilha.php
+++ PHP_Error/pilha.php
@@ -18,13 +18,17 @@ function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
 
-    $divisao = intdiv(5, 0);
+   /*  $divisao = intdiv(5, 0);
     $arrayFixo = new SplFixedArray(2);
     $arrayFixo[3] = 'Valor';
 
     for ($i = 1; $i <= 5; $i++) {
         echo $i . PHP_EOL;
-    }
+    } */
+
+    $exception = new RuntimeException();
+    throw $exception;
+
     echo 'Saindo da função 2' . PHP_EOL;
 }
 
